aa1e368724c1ca6f538c966d2bba61137013dbe2
Muhammad Ramadhan Fikri <55729945+ramafikri02@users.noreply.github.com>
2020-04-18 07:52:32 +0700

    Add files via upload

---
+++ report.php
@@ -0,0 +1,13 @@
+<?php 
+require 'vendor/autoload.php';
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+$dompdf->loadHtml('Menggunakan Library DOMpdf untuk Membuat Report PDF dengan DOMpdf');
+
+$dompdf->setPaper('A4', 'landscape');
+
+$dompdf->render();
+
+$dompdf->stream('hasil_report.pdf');
+?>
